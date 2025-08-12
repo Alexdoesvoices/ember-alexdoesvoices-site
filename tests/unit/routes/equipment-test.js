dd42a3dc08cb1101c9b847e58bfcb7410e7cf707
Alexdoesvoice <alexrrobinson05@gmail.com>
2025-08-12 14:01:06 -0500

    Ember  Test

---
+++ tests/unit/routes/equipment-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-alexdoesvoices-site/tests/helpers';
+
+module('Unit | Route | equipment', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists', function (assert) {
+    let route = this.owner.lookup('route:equipment');
+    assert.ok(route);
+  });
+});
